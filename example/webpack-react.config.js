e6873ca52945b7fd86afaa55f51c8579ffb70fca
Michael Pearson <mipearson@gmail.com>
2015-07-31 08:15:25 +1000

    Update examples in prep for release

--- example/webpack-react.config.js
+++ example/webpack-react.config.js
@@ -1,4 +1,5 @@
-// Example webpack configuration with asset fingerprinting in production.
+// Example webpack configuration for React with live reloading in development
+// and asset fingerprinting in production.
 'use strict';
 
 // jshint camelcase:false
@@ -10,8 +11,12 @@ var StatsPlugin = require('stats-webpack-plugin');
 // must match config.webpack.dev_server.port
 var devServerPort = 3808;
 
-// set TARGET=production on the environment to add asset fingerprints
+// set TARGET=production on the environment to add asset fingerprints &
+// omit hot-reloading
 var production = process.env.TARGET === 'production';
+
+// set NO_HOT_RELOAD=1 to disable hot reloading, even in development
+var hotReload = !production && !process.env.NO_HOT_RELOAD;
 
 var config = {
   entry: {
@@ -32,7 +37,29 @@ var config = {
     filename: production ? '[name]-[chunkhash].js' : '[name].js'
   },
 
+  module: {
+    loaders: [
+      // Example babel loader, with hot-reloading in development
+      {
+        test: /\.jsx?$/,
+        loaders: hotReload ? ['react-hot', 'babel'] : ['babel'],
+        include: path.join(__dirname, '..', 'webpack'),
+        exclude: [
+          path.join(__dirname, '..', 'webpack', 'vendor'),
+          path.join(__dirname, '..', 'webpack', 'legacy')
+        ]
+
+      },
+
+      // Expose React to the global context for use with the Chrome react plugin
+      {
+        test: require.resolve('react'), loader: 'expose?React'
+      }
+    ]
+  },
+
   resolve: {
+    extensions: ['', '.js', '.jsx'],
     root: path.join(__dirname, '..', 'webpack')
   },
 
@@ -61,6 +88,25 @@ if (production) {
   config.output.publicPath = '//localhost:' + devServerPort + '/webpack/';
   // Source maps
   config.devtool = 'cheap-module-eval-source-map';
+}
+
+if (hotReload) {
+  // react-hot-loader configuration bits
+  config.plugins = config.plugins.concat([
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]);
+  Object.keys(config.entry).forEach(function (entryPoint) {
+    if (!Array.isArray(config.entry[entryPoint])) {
+      config.entry[entryPoint] = [config.entry[entryPoint]];
+    }
+    config.entry[entryPoint] = [
+      'webpack-dev-server/client?http://0.0.0.0:' + devServerPort,
+      'webpack/hot/only-dev-server'
+    ].concat(config.entry[entryPoint]);
+  });
+  config.devServer.hot = true;
+
 }
 
 module.exports = config;
