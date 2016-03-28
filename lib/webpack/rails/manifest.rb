150657d34caf881dd474437db3c589141c2c6bba
Sichacvah <sichirc@gmail.com>
2016-03-28 14:05:27 +0500

    add manifest host from Rails.configuration

--- lib/webpack/rails/manifest.rb
+++ lib/webpack/rails/manifest.rb
@@ -54,7 +54,7 @@ module Webpack
 
         def load_dev_server_manifest
           http = Net::HTTP.new(
-            "localhost",
+            ::Rails.configuration.webpack.dev_server.host,
             ::Rails.configuration.webpack.dev_server.port)
           http.use_ssl = ::Rails.configuration.webpack.dev_server.https
           http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -81,7 +81,7 @@ module Webpack
         end
 
         def dev_server_url
-          "http://localhost:#{::Rails.configuration.webpack.dev_server.port}#{dev_server_path}"
+          "http://#{Rails.configuration.webpack.dev_server.host}:#{::Rails.configuration.webpack.dev_server.port}#{dev_server_path}"
         end
       end
     end
